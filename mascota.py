3bcebdf99cf20e6505dd11bbbb1cf5d3ea93c54f
Flor <florenciacuccarese@gmail.com>
2017-03-16 11:12:52 -0300

    1

---
+++ mascota.py
@@ -0,0 +1,12 @@
+class mascota (object):
+    nombre=""
+    tipo=""
+
+    def setnombre(self,n):
+        self.nombre=str(n)
+
+    def settipo (self,t):
+        self.tipo=str(t)
+
+    def quiensoy (self):
+        return "soy"+self.nombre+"un"+self.tipo
